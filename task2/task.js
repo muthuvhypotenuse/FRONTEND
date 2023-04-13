abd1b0df20d2dd9426f5ce8a78de19e357fed73b
muthukumar <122853909+muthuvhypotenuse@users.noreply.github.com>
2023-04-13 17:01:36 +0530

    changes is done

--- task2/task.js
+++ task2/task.js
@@ -1,20 +1,16 @@
-
 const number = 15;
-const number2= 11;
-if (number % 3 === 0) {
+const number2 = 11;
+if (number % 3 === 0 && number % 5 === 0) {
+  console.log("Foo & Bar");
+} else if (number % 3 === 0) {
   console.log("Foo");
-}
-else if (number % 5 === 0) {
+} else if (number % 5 === 0) {
   console.log("Bar");
-}
-else if (number % 3 === 0 && number % 5 === 0) {
-    console.log("Foo & Bar");
-  }
-else if (number2 % 3 !== 0 && number2 % 5 !== 0){
+} else {
   console.log("Neither Foo nor Bar");
 }
 
-const result = (number > number2) ? (number + number2) : (number - number2);
+const result = number > number2 ? number + number2 : number - number2;
 console.log(result);
-const result2 = (number < number2) ? (number + number2) : (number - number2);
+const result2 = number < number2 ? number + number2 : number - number2;
 console.log(result2);
